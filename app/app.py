51959f0f89a9cb726c455ceb5b05af03248d99b1
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-08-24 11:57:56 -0500

    Initial (#1)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

---
+++ app/app.py
@@ -0,0 +1,82 @@
+import random
+
+import data
+
+from flask import Flask, render_template
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+
+api = Api(app)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title="Home")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', title="The page you were looking for doesn't exist (404)"), 404
+
+
+class Quote(Resource):
+
+    @staticmethod
+    def get(quote_id: int = 0):
+        if quote_id == 0:
+            return random.choice(data.QUOTES), 200
+        for quote in data.QUOTES:
+            if quote["quote_id"] == quote_id:
+                return quote, 200
+        return "Quote not found", 404
+
+    @staticmethod
+    def post(quote_id: int):
+        parser = reqparse.RequestParser()
+        parser.add_argument("author")
+        parser.add_argument("quote")
+        params = parser.parse_args()
+        for quote in data.QUOTES:
+            if quote_id == quote["quote_id"]:
+                return f"Quote with id {quote_id} already exists", 400
+        quote = {
+            "id": int(quote_id),
+            "author": params["author"],
+            "quote": params["quote"]
+        }
+        data.QUOTES.append(quote)
+        return quote, 201
+
+    @staticmethod
+    def put(quote_id: int):
+        parser = reqparse.RequestParser()
+        parser.add_argument("author")
+        parser.add_argument("quote")
+        params = parser.parse_args()
+        for quote in data.QUOTES:
+            if quote_id == quote["quote_id"]:
+                quote["author"] = params["author"]
+                quote["quote"] = params["quote"]
+                return quote, 200
+
+        quote = {
+            "id": quote_id,
+            "author": params["author"],
+            "quote": params["quote"]
+        }
+
+        data.QUOTES.append(quote)
+        return quote, 201
+
+    @staticmethod
+    def delete(quote_id: int):
+        data.QUOTES = [quote for quote in data.QUOTES if quote["quote_id"] != quote_id]
+        return f"Quote with id {quote_id} is deleted.", 200
+
+
+api.add_resource(Quote, "/api/v1/quotes", "/api/v1/quotes/", "/api/v1/quotes/<int:quote_id>")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/data.py
@@ -0,0 +1,63 @@
+QUOTES = [
+    {
+        "quote_id": 0,
+        "author": "Dr. Seuss",
+        "quote": "Don't cry because it's over, smile because it happened."
+    },
+    {
+        "quote_id": 1,
+        "author": "Oscar Wilde",
+        "quote": "Be yourself; everyone else is already taken."
+    },
+    {
+        "quote_id": 2,
+        "author": "Albert Einstein",
+        "quote": "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
+    },
+    {
+        "quote_id": 3,
+        "author": "Marilyn Monroe",
+        "quote": "I'm selfish, impatient and a little insecure. " +
+                 "I make mistakes, I am out of control and at times hard to handle. " +
+                 "But if you can't handle me at my worst, then you sure as hell don't deserve me at my best."
+    },
+    {
+        "quote_id": 4,
+        "author": "Frank Zappa",
+        "quote": "So many books, so little time."
+    },
+    {
+        "quote_id": 5,
+        "author": "Bernard M. Baruch",
+        "quote": "Be who you are and say what you feel, because those who mind don't matter, " +
+                 "and those who matter don't mind."
+    },
+    {
+        "quote_id": 6,
+        "author": "Marcus Tullius Cicero",
+        "quote": "A room without books is like a body without a soul."
+    },
+    {
+        "quote_id": 7,
+        "author": "William W. Purkey",
+        "quote": "You've gotta dance like there's nobody watching, " +
+                 "Love like you'll never be hurt, " +
+                 "Sing like there's nobody listening, " +
+                 "And live like it's heaven on earth."
+    },
+    {
+        "quote_id": 8,
+        "author": "Mahatma Gandhi",
+        "quote": "Be the change that you wish to see in the world."
+    },
+    {
+        "quote_id": 9,
+        "author": "Robert Frost",
+        "quote": "In three words I can sum up everything I've learned about life: it goes on."
+    },
+    {
+        "quote_id": 10,
+        "author": "Mark Twain",
+        "quote": "If you tell the truth, you don't have to remember anything."
+    }
+]
