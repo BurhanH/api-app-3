d1cc11c59b9412b06eebd6d8f9c8a65c77140239
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-08-24 16:16:36 -0500

    Update data.py

--- app/data.py
+++ app/data.py
@@ -1,44 +1,44 @@
 QUOTES = [
     {
-        "quote_id": 0,
+        "quote_id": 1,
         "author": "Dr. Seuss",
         "quote": "Don't cry because it's over, smile because it happened."
     },
     {
-        "quote_id": 1,
+        "quote_id": 2,
         "author": "Oscar Wilde",
         "quote": "Be yourself; everyone else is already taken."
     },
     {
-        "quote_id": 2,
+        "quote_id": 3,
         "author": "Albert Einstein",
         "quote": "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe."
     },
     {
-        "quote_id": 3,
+        "quote_id": 4,
         "author": "Marilyn Monroe",
         "quote": "I'm selfish, impatient and a little insecure. " +
                  "I make mistakes, I am out of control and at times hard to handle. " +
                  "But if you can't handle me at my worst, then you sure as hell don't deserve me at my best."
     },
     {
-        "quote_id": 4,
+        "quote_id": 5,
         "author": "Frank Zappa",
         "quote": "So many books, so little time."
     },
     {
-        "quote_id": 5,
+        "quote_id": 6,
         "author": "Bernard M. Baruch",
         "quote": "Be who you are and say what you feel, because those who mind don't matter, " +
                  "and those who matter don't mind."
     },
     {
-        "quote_id": 6,
+        "quote_id": 7,
         "author": "Marcus Tullius Cicero",
         "quote": "A room without books is like a body without a soul."
     },
     {
-        "quote_id": 7,
+        "quote_id": 8,
         "author": "William W. Purkey",
         "quote": "You've gotta dance like there's nobody watching, " +
                  "Love like you'll never be hurt, " +
@@ -46,17 +46,17 @@ QUOTES = [
                  "And live like it's heaven on earth."
     },
     {
-        "quote_id": 8,
+        "quote_id": 9,
         "author": "Mahatma Gandhi",
         "quote": "Be the change that you wish to see in the world."
     },
     {
-        "quote_id": 9,
+        "quote_id": 10,
         "author": "Robert Frost",
         "quote": "In three words I can sum up everything I've learned about life: it goes on."
     },
     {
-        "quote_id": 10,
+        "quote_id": 11,
         "author": "Mark Twain",
         "quote": "If you tell the truth, you don't have to remember anything."
     }
